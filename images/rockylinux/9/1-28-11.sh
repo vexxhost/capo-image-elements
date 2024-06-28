e66c1810740415777462b80524341f7f2bf76aef
Mohammed Naser <mnaser@vexxhost.com>
2024-06-28 21:43:44 +0000

    Add more changes

--- images/rockylinux/9/1-28-11.sh
+++ images/rockylinux/9/1-28-11.sh
@@ -4,4 +4,5 @@
 export ELEMENTS_PATH=$PWD/elements
 export DIB_RELEASE=9
 export DIB_KUBERNETES_VERSION=1.28.11
-disk-image-create vm rocky-container kubernetes
+export DIB_CLOUD_INIT_GROWPART_DEVICES='["/"]'
+disk-image-create vm rocky-container cloud-init cloud-init-growpart kubernetes
