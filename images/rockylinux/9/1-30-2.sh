dd148da98e7552b3da7850c6d3e4fe2f24b43ce2
Tadas Sutkaitis <tadasas@gmail.com>
2025-03-19 01:22:08 +0200

    feat: finalize changes

--- images/rockylinux/9/1-30-2.sh
+++ images/rockylinux/9/1-30-2.sh
@@ -3,7 +3,6 @@
 
 export ELEMENTS_PATH=$PWD/elements
 export DIB_RELEASE=9
-export DIB_BOOTLOADER_DEFAULT_CMDLINE="transparent_hugepage=madvise"
 export DIB_KUBERNETES_VERSION=1.30.2
 export DIB_CLOUD_INIT_GROWPART_DEVICES='["/"]'
-disk-image-create vm rocky-container bootloader epel cloud-init cloud-init-growpart kubernetes
+disk-image-create vm block-device-efi rocky-container selinux-permissive epel pkgs-rpm cloud-init cloud-init-growpart kubernetes
--- images/rockylinux/9/1-31-1.sh
+++ images/rockylinux/9/1-31-1.sh
@@ -3,5 +3,6 @@
 
 export ELEMENTS_PATH=$PWD/elements
 export DIB_RELEASE=9
-export DIB_KUBERNETES_VERSION=1.29.6
+export DIB_KUBERNETES_VERSION=1.31.1
+export DIB_CLOUD_INIT_GROWPART_DEVICES='["/"]'
 disk-image-create vm block-device-efi rocky-container selinux-permissive epel pkgs-rpm cloud-init cloud-init-growpart kubernetes
