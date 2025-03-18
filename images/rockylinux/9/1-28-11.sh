ebb6ea701f279e9e262d6c9ea70897007d5e4c38
Tadas Sutkaitis <tadasas@gmail.com>
2025-03-19 01:36:36 +0200

    fix: add missing dib for old k8s versions

--- images/rockylinux/9/1-28-11.sh
+++ images/rockylinux/9/1-28-11.sh
@@ -4,4 +4,5 @@
 export ELEMENTS_PATH=$PWD/elements
 export DIB_RELEASE=9
 export DIB_KUBERNETES_VERSION=1.28.11
+export DIB_CLOUD_INIT_GROWPART_DEVICES='["/"]'
 disk-image-create vm block-device-efi rocky-container selinux-permissive epel pkgs-rpm cloud-init cloud-init-growpart kubernetes
