b035a083c081023dbf82d4e6d4c819d0432d8a0c
Mohammed Naser <mnaser@vexxhost.com>
2025-04-07 20:30:45 +0000

    Cleanup + add Rocky

--- images/rockylinux/9/1-30-2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# SPDX-License-Identifier: Apache-2.0
-
-export ELEMENTS_PATH=$PWD/elements
-export DIB_RELEASE=9
-export DIB_KUBERNETES_VERSION=1.30.2
-disk-image-create vm block-device-efi rocky-container kubernetes
--- images/ubuntu/jammy/1-27-15.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# SPDX-License-Identifier: Apache-2.0
-# Image-Rebuild-Time: 2024-06-28T18:14:42Z
-
-export ELEMENTS_PATH=$PWD/elements
-export DIB_RELEASE=jammy
-export DIB_KUBERNETES_VERSION=1.27.15
-disk-image-create vm block-device-efi ubuntu kubernetes
