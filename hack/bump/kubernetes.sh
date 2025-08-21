1fcf85b0bbca512c1cb58da913dd3471d2d6b01d
Mohammed Naser <mnaser@vexxhost.com>
2025-08-21 13:10:35 +0000

    ci: add bump to ci
    Signed-off-by: Mohammed Naser <mnaser@vexxhost.com>

---
+++ hack/bump/kubernetes.sh
@@ -0,0 +1,33 @@
+set -e
+
+# Fetch current maintained Kubernetes versions
+VERSIONS=$(curl -s https://endoflife.date/api/v1/products/kubernetes | jq -r '.result.releases[] | select(.isMaintained == true).latest.name' | sort -V)
+
+# Build the version list for YAML
+VERSION_LINES=""
+for version in $VERSIONS; do
+    VERSION_LINES="${VERSION_LINES}          - ${version}\n"
+done
+
+# Update CI workflow in place using awk
+awk -i inplace -v versions="$VERSION_LINES" '
+/^        version:$/ {
+    print
+    printf "%s", versions
+    in_version = 1
+    next
+}
+in_version && /^          -/ {
+    # Skip old version lines
+    next
+}
+in_version && !/^          -/ {
+    in_version = 0
+}
+!in_version {
+    print
+}
+' .github/workflows/ci.yaml
+
+echo "Updated Kubernetes versions in CI workflow to:"
+echo "$VERSIONS"
